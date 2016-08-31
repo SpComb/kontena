2d8bf5683220d0424f5124a4b305961010b52295
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-08-31 15:29:49 +0300

    Created a rake task to reset local admin

--- server/app/initializers/mongo_pubsub.rb
+++ server/app/initializers/mongo_pubsub.rb
@@ -2,4 +2,6 @@ require_relative 'mongoid'
 require_relative '../services/mongo_pubsub'
 require_relative '../models/pubsub_channel'
 
-MongoPubsub.start!(PubsubChannel.collection) if ENV['RACK_ENV'] != 'test'
+unless ENV['RACK_ENV'] == 'test' || ENV['NO_MONGO_PUBSUB']
+  MongoPubsub.start!(PubsubChannel.collection)
+end
